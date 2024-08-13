4b266e627aade7757b7584259e49683f6de8e4a4
Ivan Malison <IvanMalison@gmail.com>
2024-08-12 22:53:26 -0600

    Fix a couple of lint issues

--- org-window-habit.el
+++ org-window-habit.el
@@ -6,7 +6,7 @@
 ;; Keywords: calendar org-mode habit interval window
 ;; URL: https://github.com/colonelpanic8/org-window-habit
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "28.1") (org "9.0.0") (dash "2.10.0"))
+;; Package-Requires: ((emacs "29.1") (org "9.0.0") (dash "2.10.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -973,7 +973,6 @@ If LINE is provided, insert graphs at beggining of line"
             without-completion-assessment-value)))
          (completion-today-matters
           (< without-completion-assessment-value with-completion-assessment-value))
-         (interval-is-present (eq current-interval-time-type 'present))
          (next-required-interval (org-window-habit-get-next-required-interval habit))
          (completion-expected-today
           (org-window-habit-time-falls-in-assessment-interval
